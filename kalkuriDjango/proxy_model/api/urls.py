fb95a188fce91e82f7b970573fbdb8cdd25f122d
SaiKalkuri <sai.kalkuri@gmail.com>
2024-04-14 15:49:20 +0530

    devops

--- kalkuriDjango/proxy_model/api/urls.py
+++
@@ -1,8 +0,0 @@
-from api.views import ProductViewSet,ProductProxyViewSet
-from rest_framework.routers import DefaultRouter
-router = DefaultRouter()
-router.register(r'product', ProductViewSet, basename="product")
-router.register(r'productproxy', ProductProxyViewSet, basename="productproxy")
-urlpatterns = [
-    
-]+router.urls
